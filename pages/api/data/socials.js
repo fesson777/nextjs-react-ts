5e1139f6bc948f77a553cbc70343b28f96e9cf3e
fesson <cccp0110@gmail.com>
2022-04-22 15:15:17 +0300

    next-js-react

---
+++ pages/api/data/socials.js
@@ -0,0 +1,17 @@
+export const socials = [
+  {
+    id: 1,
+    icon: 'github',
+    path: 'https://github.com/fesson777',
+  },
+  {
+    id: 2,
+    icon: 'vk',
+    path: 'https://vk.com/fesson',
+  },
+  {
+    id: 3,
+    icon: 'twitter',
+    path: 'https://twitter.com/',
+  },
+]
---
+++ pages/api/socials.js
@@ -0,0 +1,8 @@
+import { NextApiRequest, NextApiResponse } from 'next'
+import { socials } from './data/socials'
+
+export default function handler(req, res) {
+  if (req.method === 'GET') {
+    res.status(200).json(socials)
+  }
+}
